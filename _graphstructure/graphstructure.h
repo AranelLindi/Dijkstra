870517ad45cf716b2763f434a7340d9bb2f8c437
Stefan Lindörfer <stefan.lindoerfer@me.com>
2020-05-20 17:17:16 +0200

    Erster Commit. Noch funktionier nichts.

---
+++ _graphstructure/graphstructure.h
@@ -0,0 +1,16 @@
+#ifndef GRAPHSTRUCTURE_H
+#define GRAPHSTRUCTURE_H
+
+#include "rgraphml.h"
+
+
+class GraphStructure {
+
+    GraphStructure(const GraphML& graph); // Konstruktor
+    ~GraphStructure(void); // Destruktor
+
+    
+};
+
+
+#endif // graphstructure.h
